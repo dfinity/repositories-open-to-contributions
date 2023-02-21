3aee67ae1f356c9bee0760cd2cf1bb90a6da03df
Carly Gundy <carlene.gundy@dfinity.org>
2023-02-21 16:50:58 +0100

    rewrite sort in python

---
+++ custom_python_actions/check_sort.py
@@ -0,0 +1,10 @@
+def check_sort():
+    with open("open-repositories.txt") as my_file:
+        data = my_file.read()
+        repo_list = data.replace("\n", " ").split(".")
+        if repo_list != sorted(repo_list):
+            raise Exception("open-repositories.txt is not correctly sorted")
+
+
+if __name__ == "__main__":
+    check_sort()

5ca3024e4bd2bfe328417d58b2d32e95271dd9cd
Carly Gundy <carlene.gundy@dfinity.org>
2023-02-21 16:56:06 +0100

    debug

--- custom_python_actions/check_sort.py
+++ custom_python_actions/check_sort.py
@@ -2,6 +2,8 @@ def check_sort():
     with open("open-repositories.txt") as my_file:
         data = my_file.read()
         repo_list = data.replace("\n", " ").split(".")
+        print(repo_list)
+        print(sorted(repo_list))
         if repo_list != sorted(repo_list):
             raise Exception("open-repositories.txt is not correctly sorted")
 

2128132dad033ea24f7ab099e47172eabef13f39
Carly Gundy <carlene.gundy@dfinity.org>
2023-03-15 11:59:36 +0100

    we forgot the file

---
+++ custom_python_actions/utils.py
@@ -0,0 +1,23 @@
+import time
+
+import github3
+
+
+def download_gh_file(repo: github3.github.repo, file_path: str) -> str:
+    # sometimes the request does not work the first time, so set a retry
+    for attempt in range(5):
+        try:
+            file_content = repo.file_contents(file_path)
+            break
+        except ConnectionResetError as error:
+            if attempt < 4:
+                time.sleep(3)
+                continue
+            else:
+                # if it hasn't succeeded after 5 tries, raise the error
+                raise Exception(
+                    "Error downloading data. Try rerunning the CI job"
+                ) from error
+
+    file_decoded = file_content.decoded.decode()
+    return file_decoded

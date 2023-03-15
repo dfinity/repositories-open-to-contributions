0a8fdf99472f04de205b6e31fa8c217805d68d88
Carly Gundy <carlene.gundy@dfinity.org>
2023-03-15 17:52:20 +0100

    add code to check permissions

--- custom_python_actions/check_compliance.py
+++ custom_python_actions/check_compliance.py
@@ -1,20 +1,28 @@
 import os
+import re
+import sys
+from typing import Union
 
 import github3
 
 
-def check_code_owners(repo: github3.github.repo) -> bool:
+def get_code_owners(repo: github3.github.repo) -> Union[str, None]:
     valid_codowner_paths = [
         "/CODEOWNERS",
         "/.github/CODEOWNERS",
     ]
     for path in valid_codowner_paths:
         try:
-            repo.file_contents(path)
-            return True
+            code_owners_file = repo.file_contents(path)
+            code_owners = code_owners_file.decoded.decode()
+            return code_owners
         except github3.exceptions.NotFoundError:
             pass
-    return False
+    return None
+
+
+def check_code_owners(repo: github3.github.repo) -> bool:
+    return True if get_code_owners(repo) else False
 
 
 def check_branch_protection(repo: github3.github.repo) -> bool:
@@ -38,14 +46,61 @@ def check_readme(repo: github3.github.repo) -> bool:
         return False
 
 
+def get_team_name(code_owners: str, org_name: str) -> str:
+    code_owner_file_lines = code_owners.split("\n")
+    for line in code_owner_file_lines:
+        # If anyone is a regex wizard, please help me out here!
+        if "#" in line:
+            team_handle = re.findall(r"(?=\*. *@).*(@.*)(?= *#)(?<=\w)", line)
+        else:
+            team_handle = re.findall(r"(?=\*. *@).*(@.*)", line)
+        if len(team_handle) > 0:
+            print(f"Codeowners found {team_handle}")
+            break
+    if len(team_handle) > 1:
+        print("Only one team can be listed for repo-level codeowners.")
+        sys.exit(1)
+    if len(team_handle) == 0:
+        print(
+            "No repo-level team owner found. Double check the format of your CODEOWNERS file."
+        )
+        sys.exit(1)
+    codeowner_team = team_handle[0]
+    team_name = codeowner_team.strip(f"@{org_name}").strip("/")
+
+    return team_name
+
+
+def check_permissions(repo_name: str, team_name: str, org: github3.github.orgs) -> bool:
+    try:
+        role = (
+            org.team_by_name(team_name)
+            .permissions_for(f"{org.name}/{repo_name}")
+            .role_name
+        )
+    except github3.exceptions.NotFoundError:
+        print("Repository Permissions could not be found")
+        return False
+
+    if role in ["admin", "write"]:
+        print(f"Team {team_name} has {role} permissions.")
+        return True
+    else:
+        print(
+            f"Insufficient permissions. Requires write or admin, but team {team_name} has {role}."
+        )
+        return False
+
+
 def main() -> None:
-    org = os.environ["GH_ORG"]
+    org_name = os.environ["GH_ORG"]
     gh_token = os.environ["GH_TOKEN"]
     repo_name = os.environ["REPO"]
 
     gh = github3.login(token=gh_token)
     try:
-        repo = gh.repository(owner=org, repository=repo_name)
+        repo = gh.repository(owner=org_name, repository=repo_name)
+        org = gh.organization(username=org_name)
     except github3.exceptions.NotFoundError as e:
         raise Exception(
             f"Github repo {repo_name} not found. Double check the spelling and that your repository is public."  # noqa
@@ -56,10 +111,18 @@ def main() -> None:
     has_license = check_license(repo)
     is_branch_protected = check_branch_protection(repo)
 
+    if has_codeowners:
+        code_owners = get_code_owners()
+        team_name = get_team_name(code_owners, org_name)
+        has_permissions = check_permissions(repo_name, team_name, org)
+    else:
+        has_permissions = False
+
     os.system(f"""echo 'has_codeowners={has_codeowners}' >> $GITHUB_OUTPUT""")
     os.system(f"""echo 'has_readme={has_readme}' >> $GITHUB_OUTPUT""")
     os.system(f"""echo 'has_license={has_license}' >> $GITHUB_OUTPUT""")
     os.system(f"""echo 'is_branch_protected={is_branch_protected}' >> $GITHUB_OUTPUT""")
+    os.system(f"""echo 'has_permissions={has_permissions}' >> $GITHUB_OUTPUT""")
 
 
 if __name__ == "__main__":
--- tests/test_check_compliance.py
+++ tests/test_check_compliance.py
@@ -9,38 +9,64 @@ from custom_python_actions.check_compliance import (
     check_code_owners,
     check_license,
     check_readme,
+    get_code_owners,
+    get_team_name,
 )
 
 
-def test_check_code_owners_succeeds():
+def test_check_get_code_owners_succeeds():
     repo = mock.Mock()
-    code_owners_file = "* @dfinity/idx\n"
+    code_owners_file = mock.Mock()
+    code_owners_file.decoded.decode.return_value = "* @dfinity/idx\n"
     repo.file_contents = mock.Mock(
         side_effect=[NotFoundError(mock.Mock()), code_owners_file]
     )
 
-    code_owners_check = check_code_owners(repo)
+    code_owners = get_code_owners(repo)
 
     assert repo.file_contents.call_count == 2
+    assert code_owners_file.decoded.decode.call_count == 1
     repo.file_contents.assert_has_calls(
         [mock.call("/.github/CODEOWNERS"), mock.call("/CODEOWNERS")], any_order=True
     )
-    assert code_owners_check == True
+    assert code_owners == "* @dfinity/idx\n"
 
 
-def test_check_code_owners_fails():
+def test_check_get_code_owners_fails():
     repo = mock.Mock()
     repo.file_contents = mock.Mock(
         side_effect=[NotFoundError(mock.Mock()), NotFoundError(mock.Mock())]
     )
 
-    code_owners_check = check_code_owners(repo)
+    code_owners = get_code_owners(repo)
 
     assert repo.file_contents.call_count == 2
     repo.file_contents.assert_has_calls(
         [mock.call("/.github/CODEOWNERS"), mock.call("/CODEOWNERS")], any_order=True
     )
-    assert code_owners_check == False
+    assert code_owners == None
+
+
+def test_check_code_owners_succeeds():
+    repo = mock.Mock()
+    code_owners_file = mock.Mock()
+    code_owners_file.decoded.decode.return_value = "* @dfinity/idx\n"
+    repo.file_contents.return_value = code_owners_file
+
+    check_succeeds = check_code_owners(repo)
+
+    assert repo.file_contents.call_count == 1
+    assert check_succeeds == True
+
+
+def test_check_code_owners_fails():
+    repo = mock.Mock()
+    repo.file_contents.side_effect = NotFoundError(mock.Mock())
+
+    check_succeeds = check_code_owners(repo)
+
+    assert repo.file_contents.call_count == 2
+    assert check_succeeds == False
 
 
 def test_check_license_exists():
@@ -97,3 +123,26 @@ def test_check_readme_other_error():
 
     with pytest.raises(Exception):
         readme = check_readme(repo)
+
+
+code_owners_test_file_1 = "* @dfinity/idx\n"
+code_owners_test_file_2 = "* @another-org/another-team # some comment"
+
+
+def code_owners_test_file_3():
+    code_owners = open("tests/test_data/CODEOWNERS3", "r").read()
+    return code_owners
+
+
+@pytest.mark.parametrize(
+    "test_input,org_name,expected",
+    [
+        (code_owners_test_file_1, "dfinity", "idx"),
+        (code_owners_test_file_2, "another-org", "another-team"),
+        (code_owners_test_file_3(), "dfinity-lab", "some-team"),
+    ],
+)
+def test_get_team_name_succeeds(test_input, org_name, expected):
+    team_name = get_team_name(test_input, org_name)
+
+    assert team_name == expected

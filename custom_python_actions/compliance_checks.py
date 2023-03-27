acda19806512ad7ac596ead87c45e5c6010a9e43
Carly Gundy <carlene.gundy@dfinity.org>
2023-03-27 10:11:09 +0200

    fix linting error

--- custom_python_actions/compliance_checks.py
+++ custom_python_actions/compliance_checks.py
@@ -115,7 +115,8 @@ class RepoPermissions(ComplianceCheck):
             self.succeeds = True
             self.message = f"Team {team_name} has {role} permissions."
         else:
-            self.message = f"Insufficient permissions. Requires write or maintain, but team {team_name} has {role}."
+            self.message = f"Insufficient permissions. Requires write or \
+            maintain, but team {team_name} has {role}."
 
 
 @dataclass

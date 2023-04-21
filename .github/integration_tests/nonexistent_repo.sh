fd3a957a919cb19e9bf1d060a01709a5427806dd
Carly Gundy <carlene.gundy@dfinity.org>
2023-04-21 13:57:52 +0200

    try out everything

--- .github/integration_tests/nonexistent_repo.sh
+++ .github/integration_tests/nonexistent_repo.sh
@@ -3,14 +3,10 @@
 # Integration test to ensure CI fails when a non-existent repository is added
 
 # set git credentials
-# git config user.name github-actions[bot]
-# git config user.email github-actions[bot]@users.noreply.github.com
 git config user.name dfinity
 git config user.email dfinity@dfinity.org
-# git config user.password $GH_TOKEN
 
 gh auth login
-gh auth setup-git
 
 # create new branch
 git checkout -b integration-test-1
@@ -29,6 +25,7 @@ commit_sha=$(git rev-parse HEAD)
 # check CI
 while true
 do
+    sleep 3
     status=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-1 | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .status')
     echo $status
     if [[ $status == '"completed"' ]]
@@ -45,4 +42,4 @@ else
 fi
 
 # delete branch and close PR
-#git push origin -d integration-test-1
+git push origin -d integration-test-1

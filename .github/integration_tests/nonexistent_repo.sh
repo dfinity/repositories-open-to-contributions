258de418887e8816d12e2b39fe4cf4e3ee74aec6
Carly Gundy <carlene.gundy@dfinity.org>
2023-04-21 13:55:33 +0200

    syntax error

--- .github/integration_tests/nonexistent_repo.sh
+++ .github/integration_tests/nonexistent_repo.sh
@@ -29,14 +29,14 @@ commit_sha=$(git rev-parse HEAD)
 # check CI
 while true
 do
-    status=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-1 | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .status')
+    status=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-1 | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .status')
     echo $status
     if [[ $status == '"completed"' ]]
     then
         break
     fi
 done
-result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-1 | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
+result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-1 | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
 if [[ $result == '"failure"' ]]
     echo $result
     then echo "test passed"

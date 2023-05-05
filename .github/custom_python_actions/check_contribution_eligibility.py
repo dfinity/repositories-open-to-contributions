09d009dd821bdc11b59919d852f6b987077ac854
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-05 10:40:33 +0200

    save progress

---
+++ .github/custom_python_actions/check_contribution_eligibility.py
@@ -0,0 +1,84 @@
+import os
+
+import github3
+
+import messages
+from utils import download_gh_file
+
+
+def is_member_of_org(gh, org, user):
+    """
+    Return whether the user is a member of the organisation.
+    """
+    return gh.organization(org).is_member(user)
+
+
+class CLAHandler:
+    def __init__(self, gh, org):
+        self.cla_repo = gh.repository(owner=org, repository="cla")
+        self.cla_file = "CLA.md"
+
+    def cla_signed(self, issue, user):
+        for comment in issue.comments():
+            if comment.user.login == user:
+                agreement_message = messages.USER_AGREEMENT_MESSAGE.format(user)
+                comment_body = comment.body.strip()
+                if comment_body == agreement_message:
+                    print(f"CLA has been agreed to by {user}")
+                    return True
+                else:
+                    print(f"Comment created by {user} does not match CLA agreement.")
+        print(f"CLA is pending for {user}")
+        return False
+
+    def get_cla_issue(self, user):
+        for issue in self.cla_repo.issues():
+            if issue.title == f"cla: @{user}":
+                return issue
+        print(f"No CLA issue for {user}")
+
+    def create_cla_issue(self, user):
+        cla_link = f"{self.cla_repo.html_url}/blob/main/{self.cla_file}"
+        user_agreement_message = messages.USER_AGREEMENT_MESSAGE.format(user)
+        issue = self.cla_repo.create_issue(
+            f"cla: @{user}",
+            body=messages.CLA_AGREEMENT_MESSAGE.format(
+                user, cla_link, user_agreement_message
+            ),
+            labels=["cla:pending"],
+        )
+        return issue
+
+
+def main():
+    org = os.environ["GH_ORG"]
+    gh_token = os.environ["GH_TOKEN"]
+    user = os.environ["GH_USER"]
+
+    gh = github3.login(token=gh_token)
+    pr = gh.pull_request(org, repo, pr_id)
+
+    is_member = is_member_of_org(gh, org, user)
+
+    can_contribute = False
+
+    cla = CLAHandler(gh, "dfinity")
+
+    if not is_member:
+        pr.issue().add_labels("external_contributor")
+        issue = cla.get_cla_issue(user)
+        if not issue:
+            issue = cla.create_cla_issue(user)
+            pr_comment = messages.CLA_MESSAGE.format(user, issue.html_url)
+            pr.create_comment(pr_comment)
+
+        if cla.cla_signed(issue, user):
+            can_contribute = True
+    else:
+        can_contribute = True
+
+    os.system(f"""echo 'can_contribute={can_contribute}' >> $GITHUB_OUTPUT""")
+
+
+if __name__ == "__main__":
+    main()
---
+++ .github/custom_python_actions/messages.py
@@ -0,0 +1,33 @@
+# Messages
+CLOSING_MESSAGE = """Dear @{},
+
+This repository does not accept external contributions yet.
+
+We are therefore closing this Pull Request, thank you for your understanding.
+
+
+— The DFINITY Foundation"""
+
+USER_AGREEMENT_MESSAGE = "I, {}, hereby agree to the DFINITY Foundation's CLA."
+
+CLA_MESSAGE = """Dear @{},
+
+In order to potentially merge your code in this open-source repository and therefore proceed with your contribution, we need to have your approval on DFINITY's CLA({}).
+
+If you decide to agree with it, please visit this [issue]({}) and read the instructions there.
+
+— The DFINITY Foundation"""
+
+CLA_AGREEMENT_MESSAGE = """Dear @{},
+
+In order to potentially merge your code in this open-source repository and therefore proceed with your contribution, we need to have your approval on the following [CLA]({}).
+
+If you decide to agree with it, please reply with the following message:
+> {}
+
+— The DFINITY Foundation"""
+
+AGREED_MESSAGE = """Dear @{},
+
+As you have agreed to the CLA, I added the `cla:agreed` label to your PR and we are now able to proceed with your contribution."""
+PENDING_MESSAGE = """No agreement message has been found. Please visit this [issue]({}) and sign the CLA agreement."""

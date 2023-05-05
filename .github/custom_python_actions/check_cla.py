86a2aab71fd0562faa1c450976ef021d5ccfbc71
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-05 14:05:10 +0200

    add cla link

--- .github/custom_python_actions/check_cla.py
+++ .github/custom_python_actions/check_cla.py
@@ -15,7 +15,7 @@ def is_member_of_org(gh, org, user):
 class CLAHandler:
     def __init__(self, gh, org):
         self.cla_repo = gh.repository(owner=org, repository="cla")
-        self.cla_file = "CLA.md"
+        self.cla_link = f"{self.cla_repo.html_url}/blob/main/CLA.md"
 
     def cla_signed(self, issue, user):
         for comment in issue.comments():
@@ -37,12 +37,11 @@ class CLAHandler:
         print(f"No CLA issue for {user}")
 
     def create_cla_issue(self, user):
-        cla_link = f"{self.cla_repo.html_url}/blob/main/{self.cla_file}"
         user_agreement_message = messages.USER_AGREEMENT_MESSAGE.format(user)
         issue = self.cla_repo.create_issue(
             f"cla: @{user}",
             body=messages.CLA_AGREEMENT_MESSAGE.format(
-                user, cla_link, user_agreement_message
+                user, self.cla_link, user_agreement_message
             ),
             labels=["cla:pending"],
         )
@@ -73,7 +72,7 @@ def main():
         issue = cla.get_cla_issue(user)
         if not issue:
             issue = cla.create_cla_issue(user)
-            pr_comment = messages.CLA_MESSAGE.format(user, issue.html_url)
+            pr_comment = messages.CLA_MESSAGE.format(user, cla.cla_link, issue.html_url)
             pr.create_comment(pr_comment)
 
         if cla.cla_signed(issue, user):

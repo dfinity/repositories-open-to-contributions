c79b5ccc217cacbbe60b0c506b83e33fa8661756
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-08 14:23:11 +0200

    add membership tests

---
+++ .github/tests/test_membership.py
@@ -0,0 +1,22 @@
+import os
+from unittest import mock
+
+from custom_python_actions.check_membership import main
+
+
+@mock.patch.dict(
+    os.environ, {"GH_ORG": "dfinity", "GH_TOKEN": "secret", "USER": "username"}
+)
+@mock.patch("github3.login")
+def test_end_to_end(github_login_mock, capfd):
+    gh = mock.Mock()
+    gh.organization.is_member.return_value = True
+    github_login_mock.return_value = gh
+
+    main()
+    out, err = capfd.readouterr()
+
+    github_login_mock.assert_called_with(token="secret")
+    gh.organization.assert_called_with("dfinity")
+    gh.organization().is_member.assert_called_with("username")
+    assert out == "User is member of org and can contribute.\n"

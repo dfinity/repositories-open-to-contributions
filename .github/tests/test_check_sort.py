b52ef7e1cc1a4e38fd42981462b9965bcea7013c
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-08 13:47:05 +0200

    add tests for check_sort

--- .github/tests/test_check_sort.py
+++ .github/tests/test_check_sort.py
@@ -1,2 +1,21 @@
-def test_place_holder():
-    pass
+import io
+import pytest
+import sys
+
+from custom_python_actions.check_sort import check_sort
+
+
+def test_check_sort_succeeds():
+    check_sort(".github/tests/test_data/open-repositories-1.txt")
+
+
+def test_check_sort_fails():
+    filename = ".github/tests/test_data/open-repositories-2.txt"
+    errorMessage = f"{filename} is not correctly sorted"
+
+    with pytest.raises(Exception):
+        capturedOutput = io.StringIO()
+        check_sort(filename)
+        sys.stdout = capturedOutput
+
+        assert capturedOutput.getvalue() == errorMessage

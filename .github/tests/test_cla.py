06c63429bbf50998ef2e17b11acaca30d592061d
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-09 17:13:36 +0200

    add tests

--- .github/tests/test_cla.py
+++ .github/tests/test_cla.py
@@ -114,3 +114,141 @@ def test_create_cla_issue():
     cla_repo.create_issue.assert_called_with(
         "cla: @username", body=cla_agreement_message, labels=["cla:pending"]
     )
+
+
+def test_handle_cla_signed_with_agreed_label():
+    issue = mock.Mock()
+    label = mock.Mock()
+    label.name = "cla:agreed"
+    issue.original_labels = [label]
+
+    cla = CLAHandler(mock.Mock())
+    cla.handle_cla_signed(issue, "username")
+
+    issue.create_comment.assert_not_called()
+    issue.remove_label.assert_not_called()
+
+
+def test_handle_cla_signed_with_pending_label():
+    issue = mock.Mock()
+    label = mock.Mock()
+    label.name = "cla:pending"
+    issue.original_labels = [label]
+    agreement_message = AGREED_MESSAGE.format("username")
+
+    cla = CLAHandler(mock.Mock())
+    cla.handle_cla_signed(issue, "username")
+
+    issue.create_comment.assert_called_with(agreement_message)
+    issue.remove_label.assert_called_once()
+    issue.add_labels.assert_called_once()
+
+
+def test_handle_cla_signed_with_no_label(capfd):
+    issue = mock.Mock()
+    issue.original_labels = []
+
+    with pytest.raises(SystemExit):
+        cla = CLAHandler(mock.Mock())
+        cla.handle_cla_signed(issue, "username")
+        out, err = capfd.readouterr()
+        assert (
+            out
+            == "No cla labels found - manually check the cla issue to see what state it is in. Exiting program.\n"
+        )
+
+
+@mock.patch.dict(
+    os.environ,
+    {"GH_ORG": "my_org", "GH_TOKEN": "secret", "REPO": "repo-name", "PR_ID": "1"},
+)
+@mock.patch("github3.login")
+@mock.patch("custom_python_actions.check_cla.CLAHandler")
+def test_end_to_end_env_vars_set(cla_mock, gh_login_mock):
+    gh = mock.Mock()
+    gh_login_mock.return_value = gh
+    pr = mock.Mock()
+    pr.user.login = "username"
+    gh.pull_request.return_value = pr
+    cla = mock.Mock()
+    cla_mock.return_value = cla
+
+    main()
+
+    gh_login_mock.assert_called_with(token="secret")
+    gh.pull_request.assert_called_with("my_org", "repo-name", "1")
+    cla_mock.assert_called_with(gh)
+    cla.get_cla_issue.assert_called_with("username")
+
+
+@mock.patch.dict(
+    os.environ,
+    {"GH_ORG": "my_org", "GH_TOKEN": "secret", "REPO": "repo-name", "PR_ID": "1"},
+)
+@mock.patch("github3.login")
+@mock.patch("custom_python_actions.check_cla.CLAHandler")
+def test_end_to_end_no_issue(cla_mock, gh_login_mock):
+    gh = mock.Mock()
+    gh_login_mock.return_value = gh
+    pr = mock.Mock()
+    gh.pull_request.return_value = pr
+    cla = mock.Mock()
+    cla.get_cla_issue.return_value = None
+    cla_mock.return_value = cla
+
+    main()
+
+    cla.create_cla_issue.assert_called_once()
+    pr.create_comment.assert_called_once()
+
+
+@mock.patch.dict(
+    os.environ,
+    {"GH_ORG": "my_org", "GH_TOKEN": "secret", "REPO": "repo-name", "PR_ID": "1"},
+)
+@mock.patch("github3.login")
+@mock.patch("custom_python_actions.check_cla.CLAHandler")
+def test_end_to_end_cla_not_signed(cla_mock, gh_login_mock, capfd):
+    gh = mock.Mock()
+    gh_login_mock.return_value = gh
+    pr = mock.Mock()
+    gh.pull_request.return_value = pr
+    cla = mock.Mock()
+    issue = mock.Mock()
+    issue.html_url = "url"
+    cla.get_cla_issue.return_value = issue
+    cla.check_if_cla_signed.return_value = False
+    cla_mock.return_value = cla
+
+    with pytest.raises(SystemExit):
+        main()
+        out, err = capfd.readouterr()
+
+        cla.create_cla_issue.assert_not_called()
+        pr.create_comment.assert_not_called()
+        cla.check_if_cla_signed.assert_called_with(issue, mock.Mock())
+        cla.handle_cla_signed.assert_not_called()
+        assert out == "The CLA has not been signed. Please sign the CLA agreement: url"
+
+
+@mock.patch.dict(
+    os.environ,
+    {"GH_ORG": "my_org", "GH_TOKEN": "secret", "REPO": "repo-name", "PR_ID": "1"},
+)
+@mock.patch("github3.login")
+@mock.patch("custom_python_actions.check_cla.CLAHandler")
+def test_end_to_end_cla_signed(cla_mock, gh_login_mock):
+    gh = mock.Mock()
+    gh_login_mock.return_value = gh
+    pr = mock.Mock()
+    pr.user.login = "username"
+    gh.pull_request.return_value = pr
+    cla = mock.Mock()
+    issue = mock.Mock()
+    cla.get_cla_issue.return_value = issue
+    cla.check_if_cla_signed.return_value = True
+    cla_mock.return_value = cla
+
+    main()
+
+    cla.handle_cla_signed.assert_called_with(issue, "username")

c9c5845b69abf3434f8a95318adbb4d56c3d4d82
Carly Gundy <carlene.gundy@dfinity.org>
2023-05-16 14:33:50 +0200

    update test

--- .github/tests/test_membership.py
+++ .github/tests/test_membership.py
@@ -5,7 +5,7 @@ from custom_python_actions.check_membership import main
 
 
 @mock.patch.dict(
-    os.environ, {"GH_ORG": "dfinity", "GH_TOKEN": "secret", "USER": "username"}
+    os.environ, {"GH_ORG": "my_org", "GH_TOKEN": "secret", "USER": "username"}
 )
 @mock.patch("github3.login")
 def test_end_to_end(github_login_mock, capfd):
@@ -17,6 +17,6 @@ def test_end_to_end(github_login_mock, capfd):
     out, err = capfd.readouterr()
 
     github_login_mock.assert_called_with(token="secret")
-    gh.organization.assert_called_with("dfinity")
+    gh.organization.assert_called_with("my_org")
     gh.organization().is_member.assert_called_with("username")
-    assert out == "User is member of org and can contribute.\n"
+    assert out == "username is member of my_org and can contribute.\n"

81fec8f9275d20edcfdf62ca874d4478eb44a3e2
Carly Gundy <carlene.gundy@dfinity.org>
2023-06-30 12:11:01 +0200

    fix

--- .github/integration_tests/compliant_repo.sh
+++ .github/integration_tests/compliant_repo.sh
@@ -34,7 +34,7 @@ do
         break
     fi
 done
-result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-integration-test-2-$RUN_NUMBER | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
+result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-2-$RUN_NUMBER | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
 if [[ $result == '"success"' ]]
     then echo "test passed"
 else

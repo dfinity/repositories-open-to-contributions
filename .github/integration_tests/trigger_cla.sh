e39d77cc7d1d266df39faf48864c2e5afebbd00e
Carly Gundy <carlene.gundy@dfinity.org>
2023-06-30 16:52:39 +0200

    add comment

--- .github/integration_tests/trigger_cla.sh
+++ .github/integration_tests/trigger_cla.sh
@@ -22,6 +22,10 @@ do
     fi
 done
 result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $AUTH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=$CURRENT_BRANCH | jq '[.workflow_runs[] | select(.name=="Check CLA")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
+
+## we expect this to fail because no PR was created and it cannot close the PR
+## However, it is enough to know that the Close PR step failed
+
 if [[ $result == '"failure"' ]]
     then echo "test passed"
 else

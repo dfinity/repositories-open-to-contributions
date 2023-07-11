23fcba4372a76b6de409c6825cb9192b6b7d44bc
Carly Gundy <carlene.gundy@dfinity.org>
2023-07-11 10:26:17 +0200

    replace branch

--- .github/integration_tests/compliant_repo.sh
+++ .github/integration_tests/compliant_repo.sh
@@ -29,14 +29,14 @@ curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bear
 while true
 do
     sleep 3
-    status=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-compliant-repo-$RUN_NUMBER | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .status')
+    status=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=$branch_name | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .status')
     echo "$status"
     if [[ "$status" == '"completed"' ]]
     then
         break
     fi
 done
-result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=integration-test-compliant-repo-$RUN_NUMBER | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
+result=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/dfinity/repositories-open-to-contributions/actions/runs?branch=$branch_name | jq '[.workflow_runs[] | select(.name=="Check New Repo")] | map(select(.head_sha=='\"$commit_sha\"')) | max_by(.run_number) | .conclusion')
 if [[ "$result" == '"success"' ]]
     then echo "test passed"
 else

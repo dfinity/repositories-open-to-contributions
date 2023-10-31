87cf8a713f84f647df4f9b026a6204f6111fc6a5
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2023-10-31 12:04:32 +0100

    Update compliance_checks.py

--- .github/custom_python_actions/compliance_checks.py
+++ .github/custom_python_actions/compliance_checks.py
@@ -116,7 +116,7 @@ class RepoPermissions(ComplianceCheck):
             self.message = f"Team {team_name} has {role} permissions."
         else:
             self.message = f"""Incorrect team-level permissions. Teams may not have admin permissions for public repositories,
-            but must have at least write or maintain permissions. Team {team_name} has {role}."""  # noqa: E501
+            but must have either write or maintain permissions. Team {team_name} has {role}."""  # noqa: E501
 
 
 @dataclass
